b812c21811ff78110644c62b75e736988b5e97f0
jmaldonadob <jmaldonadob@psl.com.co>
2020-01-28 02:34:44 -0500

    Revert "Refactor and bug fix."

--- src/main/java/com/company/bowling/core/MultiPlayerBowlingGame.java
+++ src/main/java/com/company/bowling/core/MultiPlayerBowlingGame.java
@@ -1,12 +1,11 @@
 package com.company.bowling.core;
 
-import java.util.List;
 import java.util.Map;
 
 public interface MultiPlayerBowlingGame {
 
     void roll(String playerName, String rollInputMark);
 
-    Map<String, List<Frame>> score();
+    Map<String, BowlingGame> score();
 
 }
